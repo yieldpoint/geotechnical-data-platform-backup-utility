7f916a5cdbdbd0af472f49bd62e905fbf068ac9b
Nargiza Sarkulova <nargiza@yieldpoint.com>
2017-12-13 17:01:22 -0500

    string formatting

--- backup.py
+++ backup.py
@@ -11,7 +11,8 @@ from StringIO import StringIO
 from settings import *
 
 auth = HTTPBasicAuth(USER, PASSWORD)
-base_url = 'http://%s:8000' % HOST
+base_url = 'http://{}:8000'.format(HOST)
+base_data_url = ('{}/instruments/{}/displacement-values/?format={}&start_timestamp={}')
 
 files_dir = '%s/%s' % (BACKUP_DIR, datetime.datetime.now().strftime('%m-%d-%y %H:%M:%S'))
 if not os.path.exists(files_dir):
@@ -24,15 +25,14 @@ with open('backup_status.csv') as file:
 
 backup_status_new = []
 
-instruments = json.loads(requests.get('%s/instruments' % base_url, auth=auth).text)
+instruments = json.loads(requests.get('{}/instruments'.format(base_url), auth=auth).text)
 for instrument in instruments['data']:
     instrument_id = instrument['id']
+    data_url = base_data_url.format(base_url, instrument_id, FORMAT, '')
     if INCREMENTIVE:
         if instrument_id in backup_status:
             start_timestamp = backup_status[instrument_id]
-            data_url = '%s/instruments/%s/displacement-values/?start_timestamp=%s&format=%s' % (base_url, instrument_id, start_timestamp, FORMAT)
-    else:
-        data_url = '%s/instruments/%s/displacement-values/?format=%s' % (base_url, instrument_id, FORMAT)
+            data_url = base_data_url.format(base_url, instrument_id, FORMAT, start_timestamp)
     data = requests.get(data_url, auth=auth).text
     with open('%s/%s.csv' % (files_dir, instrument_id), 'w') as file:
         file.write(data)
