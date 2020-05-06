13577a0670300ab37441b69601a6658b6114923f
oliverblake <github@oliverblake.ca>
2020-05-06 15:46:08 -0700

    Updated cron from /usr/bin/python to /usr/local/bin/python. Should fix cron problem.

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -2,6 +2,6 @@
 
 mkdir $GDP_BACKUP_DIR
 
-echo "$GDP_BACKUP_CRON_PERIOD   /usr/bin/python /opt/geotechnical-data-platform-backup-utility/backup.py" >> /etc/crontabs/root
+echo "$GDP_BACKUP_CRON_PERIOD   /usr/local/bin/python /opt/geotechnical-data-platform-backup-utility/backup.py" >> /etc/crontabs/root
 
 crond -f
