7d2157cd4d4b759b6225e5e55ca837da8742e2a3
KindarConrath <peterabrittain@gmail.com>
2020-11-30 15:09:18 -0500

    sync changes

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ GDP_BACKUP_HOST = 'localhost'
 GDP_BACKUP_USER = 'yieldpoint'
 GDP_BACKUP_PASSWORD = 'YPfuture'
 
-GDP_BACKUP_FORMAT = 'csv2'
+GDP_BACKUP_FORMAT = 'csv4'
 GDP_BACKUP_IS_INCREMENTIVE = True
 GDP_BACKUP_IS_NEW_FOLDER_PER_RUN = False
 
-GDP_BACKUP_DIR = '/home/nargiza/Downloads/backups/data'
-GDP_BACKUP_STATUS_FILE = '/home/nargiza/Downloads/backups/backup_status.csv'
+GDP_BACKUP_DIR = '/home/yieldpoint/Downloads/backups/data'
+GDP_BACKUP_STATUS_FILE = '/home/yieldpoint/Downloads/backups/backup_status.csv'
